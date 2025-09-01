9aad31f666104bf83ce98e344b000028638aa9a8
Javier Veron <javier.veron@gmail.com>
2025-09-01 00:38:28 -0300

    Fetch

--- Fetch/js/index.js
+++ Fetch/js/index.js
@@ -181,7 +181,7 @@ handlingAllPromises(); */
 
 listarProductos2(); */
 
-async function listarProductos2() {
+/* async function listarProductos2() {
     const contenido = document.getElementById("contenido");
     let contenidoHTML = "";
 
@@ -206,4 +206,112 @@ async function listarProductos2() {
     }
 }
 
-listarProductos2();
+listarProductos2(); */
+
+
+async function postearProducto() {
+    try {
+        const nombre = document.getElementById("nombre").value;
+        const precio = document.getElementById("precio").value;
+        const resultado = document.getElementById("resultado");
+    
+        const producto = {nombre, precio}; // Crear el objeto Producto (objeto javascript)
+        const response = await fetch("https://fakestoreapi.com/products", {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }, 
+            body: JSON.stringify(producto) // convertir el objeto javascript a formato json (formato texto)
+        });
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        
+        if (response.status == 201) {
+            resultado.innerHTML = `<div class="alert alert-success" role="alert">Los datos fueron Posteados correctamente!</div>`;
+        }
+    } catch (error) {
+        resultado.innerHTML = `<div class="alert alert-danger" role="alert">Error! Los datos NO FUERON Posteados correctamente!</div>`;
+    }
+}
+
+// LLAMADAS DE API
+/*
+[GET] /URL_API/PRODUCTS => TRAE TODOS LOS PRODUCTOS
+[GET] /URL_API/PRODUCTS/{ID} => TRAE EL PRODUCTO #{ID}
+[POST] /URL_API/PRODUCTS => AGREGAR UN NUEVO PRODUCTO, EL NÚMERO DE #ID LO DEVUELVE LA API
+[PUT] /URL_API/PRODUCTS/{ID} => ACTUALIZAR EL PRODUCTO #{ID}
+[DELETE] /URL_API/PRODUCTS/{ID} => ELIMINAR EL PRODUCTO #{ID}
+*/
+
+async function crearUsuario() {
+    try {
+        const nombre = document.getElementById("nombre").value;
+        const resultado = document.getElementById("resultado");
+    
+        const response = await fetch("https://playground.4geeks.com/todo/users/" + nombre, {
+            method: 'POST'
+        });
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        
+        if (response.status == 201) {
+            resultado.innerHTML = `<div class="alert alert-success" role="alert">Los datos fueron Posteados correctamente!</div>`;
+        }
+    } catch (error) {
+        resultado.innerHTML = `<div class="alert alert-danger" role="alert">Error! Los datos NO FUERON Posteados correctamente!</div>`;
+    }
+}
+
+async function listarUsuarios() {
+    const resultado = document.getElementById("resultado");
+    const response = await fetch("https://playground.4geeks.com/todo/users/");
+    const data = await response.json();
+    let contenidoHTML = "<ul class='list-group'>";
+    console.log(data);
+
+    for (const item of data.users) {        
+        contenidoHTML += `<li class="list-group-item">#${item.id}- ${item.name}</li>`;
+    }
+
+    contenidoHTML += "<ul class='list-group'>";
+    resultado.innerHTML = contenidoHTML;
+}
+
+async function crearTarea() {
+    const tarea_nombre = document.getElementById("tarea_nombre").value;
+    const tarea_completada = document.getElementById("tarea_completada").value;
+    const resultado_tareas = document.getElementById("resultado_tareas");
+    const tarea = {
+        label:tarea_nombre,
+        is_done:(tarea_completada == "true" ? true : false)
+    };
+    //console.log(tarea);
+
+    const response = await fetch("https://playground.4geeks.com/todo/todos/javierveron/", {
+        method:"POST",
+        headers: { 'Content-Type': 'application/json' }, 
+        body:JSON.stringify(tarea)
+    });
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    listarTareas();      
+}
+
+async function listarTareas() {
+    const resultado_tareas = document.getElementById("resultado_tareas");
+    const response = await fetch("https://playground.4geeks.com/todo/users/javierveron");
+    const data = await response.json();
+    let contenidoHTML = "<ul class='list-group'>";
+    console.log(data);
+
+    for (const item of data.todos) {        
+        contenidoHTML += `<li class="list-group-item">#${item.id}- ${item.label}</li>`;
+    }
+
+    contenidoHTML += "<ul class='list-group'>";
+    resultado_tareas.innerHTML = contenidoHTML;
+}
+
+listarUsuarios();
+listarTareas();
