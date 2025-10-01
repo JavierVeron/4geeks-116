45cf9324544c0e4b77c58312fce74ffc6e94b9a2
Javier Veron <javier.veron@gmail.com>
2025-10-01 06:25:24 -0300

    API con JWT

---
+++ api-jwt/app.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config["JWT_SECRET_KEY"] = "4geeks"
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "password": self.password,
+        }
+    
+@app.route("/users", methods=["GET", "POST"])
+def getOrAddUser():
+    if (request.method == "GET"):
+        result = User.query.all()
+        result = list(map(lambda x: x.serialize(), result))
+    
+        return jsonify(result)
+    elif (request.method == "POST"):
+        datos = request.get_json()
+        result = User(username=datos["username"], password=datos["password"])
+        db.session.add(result)
+        db.session.commit()
+
+        return jsonify({"estado":"ok", "mensaje":"El Usuario se agregó correctamente!"})
+
+@app.route("/users/<int:id>", methods=["GET", "DELETE"])
+def getOrDeleteUser(id):
+    result = User.query.get(id)
+
+    if (request.method == "GET"):
+        return User.serialize(result)
+    elif (request.method == "DELETE"):
+        if (result):
+            db.session.delete(result)
+            db.session.commit()
+            return jsonify({"estado":"ok", "mensaje":"El Usuario se eliminó correctamente!"})
+        else:
+            return jsonify({"estado":"error", "mensaje":"No se pudo eliminar al Usuario!"})
+
+@app.route("/token", methods=["POST"])
+def generateToken():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(username=username, password=password).first()
+
+    if user is None:
+        return jsonify({"msg": "Bad username or password"}), 401
+    
+    access_token = create_access_token(identity=str(user.id))
+
+    return jsonify({ "token": access_token, "user_id": user.id })
+
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+
+    return jsonify({"id": user.id, "username": user.username }), 200
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0')
